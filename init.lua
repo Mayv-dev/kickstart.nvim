5afc336238e47de21c4ed25827832a1fc7c3dd05
Mayv-dev <91346624+Mayv-dev@users.noreply.github.com>
2025-03-14 21:33:52 +0000

    Changed Auto Complete to Tab

--- init.lua
+++ init.lua
@@ -613,6 +613,15 @@ require('lazy').setup({
 
       local lspconfig = require 'lspconfig'
       lspconfig.gleam.setup {}
+      lspconfig.sourcekit.setup {
+        capabilities = {
+          workspace = {
+            didChangeWatchedFiles = {
+              dynamicRegistration = true,
+            },
+          },
+        },
+      }
 
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -791,7 +800,7 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<Tab>'] = cmp.mapping.confirm { select = true },
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
